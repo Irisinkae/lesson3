b487ac1bd866488db1986586d53f5bbfde2a6026
Irisinkae <irisinkae@gmail.com>
2021-11-20 21:49:49 +0300

    Add task2.py

---
+++ task2.py
@@ -0,0 +1,5 @@
+a, b, c=int(input()), int(input()), int(input())
+num1=(a**2+b**2)/(3*b-4)
+num2=(4*c**5)/6
+print(f'Целая часть равна: {num1//num2}')
+print(f'Остаток от деления равен: {num1%num2}')
