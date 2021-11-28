8e45a3ab1644702a075609274143c22522ba3e63
Irisinkae <irisinkae@gmail.com>
2021-11-28 12:42:28 +0300

    Добавлено приглашение

--- task2.py
+++ task2.py
@@ -1,4 +1,4 @@
-a, b, c=int(input()), int(input()), int(input())
+a, b, c=int(input('Введите число a: ')), int(input('Введите число b: ')), int(input('Введите число c: '))
 num1=(a**2+b**2)/(3*b-4)
 num2=(4*c**5)/6
 print(f'Целая часть равна: {num1//num2}')
